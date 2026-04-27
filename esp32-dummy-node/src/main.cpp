13adbce7d527fdd43eab54bb4e1f9b4e41ed613d
RShiro003 <pak031021@naver.com>
2026-04-27 16:13:13 +0900

    Add ESP32 timestamp to sensor payload

--- esp32-dummy-node/src/main.cpp
+++ esp32-dummy-node/src/main.cpp
@@ -1,11 +1,29 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <time.h>
 
 const char* ssid = "TP-Link_31CA";
 const char* password = "54299979";
 
 const char* serverUrl = "http://192.168.1.106:5000/api/sensor";
+
+// 한국 시간 설정
+const long gmtOffset_sec = 9 * 3600;
+const int daylightOffset_sec = 0;
+
+String getTimeString() {
+  struct tm timeinfo;
+
+  if (!getLocalTime(&timeinfo)) {
+    return "time_sync_failed";
+  }
+
+  char timeString[25];
+  strftime(timeString, sizeof(timeString), "%Y-%m-%d %H:%M:%S", &timeinfo);
+
+  return String(timeString);
+}
 
 void setup() {
   Serial.begin(115200);
@@ -22,6 +40,19 @@ void setup() {
   Serial.println("WiFi connected");
   Serial.print("ESP32 IP: ");
   Serial.println(WiFi.localIP());
+
+  // NTP 서버로 현재 시간 동기화
+  configTime(gmtOffset_sec, daylightOffset_sec, "pool.ntp.org", "time.nist.gov");
+
+  Serial.print("Time syncing");
+  while (getTimeString() == "time_sync_failed") {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.print("Current time: ");
+  Serial.println(getTimeString());
 }
 
 void loop() {
@@ -29,6 +60,11 @@ void loop() {
   float humidity = random(400, 800) / 10.0;     // 40.0 ~ 80.0
   int soilMoisture = random(0, 101);            // 0 ~ 100
   int light = random(0, 101);                   // 0 ~ 100
+
+  String timestamp = getTimeString();
+
+  Serial.print("Timestamp: ");
+  Serial.println(timestamp);
 
   Serial.print("Temperature: ");
   Serial.println(temperature);
@@ -50,6 +86,7 @@ void loop() {
 
     String jsonData = "{";
     jsonData += "\"device_id\":\"esp32_dummy\",";
+    jsonData += "\"timestamp\":\"" + timestamp + "\",";
     jsonData += "\"temperature\":" + String(temperature, 2) + ",";
     jsonData += "\"humidity\":" + String(humidity, 2) + ",";
     jsonData += "\"soil_moisture\":" + String(soilMoisture) + ",";
